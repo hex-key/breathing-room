4ebb10280dca0a1eec93b38fdcf271936a61a235
madi ! <madiggudin@gmail.com>
2023-11-11 01:43:07 -0500

    import pygame as pg

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import pygame
+import pygame as pg
 
 def main():
-    # initialize the pygame module
-    pygame.init()
+    # initialize the pg module
+    pg.init()
     # load and set the logo
-    # logo = pygame.image.load("logo32x32.png")
-    # pygame.display.set_icon(logo)
-    pygame.display.set_caption("game with story 🦣")
+    # logo = pg.image.load("logo32x32.png")
+    # pg.display.set_icon(logo)
+    pg.display.set_caption("game with story 🦣")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((1000, 750))
+    screen = pg.display.set_mode((1000, 750))
      
     # define a variable to control the main loop
     running = True
@@ -17,9 +17,9 @@ def main():
     # main loop
     while running:
         # event handling, gets all event from the event queue
-        for event in pygame.event.get():
+        for event in pg.event.get():
             # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
+            if event.type == pg.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
 
