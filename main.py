4a6c8eb135e49b5a9922508cf15801790249e9eb
madi ! <madiggudin@gmail.com>
2023-11-11 13:35:52 -0500

    changes

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pygame as pg
 class dialogue_box:
     text = ""
 
+class button:
+    text = ""
+
 def main():
     # initialize the pg module
     pg.init()
@@ -17,9 +20,8 @@ def main():
     # define a variable to control the main loop
     running = True
 
-    s = pg.Rect(50, 50, 50, 50)
-    pg.draw.rect(screen, (255, 0, 0), s)
-    pg.display.flip()
+    on_screen_objects = []
+
      
     # main loop
     while running:
@@ -30,8 +32,6 @@ def main():
                 print(chr(event.key))
             if event.type == pg.MOUSEBUTTONDOWN:
                 mouse_pos = pg.mouse.get_pos()
-                if s.collidepoint(mouse_pos):
-                    print("yay")
             if event.type == pg.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
