47a9dd2279844e9f99fa265eab39f4e820d5b120
madi ! <madiggudin@gmail.com>
2023-11-11 01:23:20 -0500

    initial commit >:)

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+def main():
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    # logo = pygame.image.load("logo32x32.png")
+    # pygame.display.set_icon(logo)
+    pygame.display.set_caption("game with story 🦣")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((1480, 960))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+if __name__ == "__main__":
+    main()
