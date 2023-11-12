3836732dc3e5a633bdfc15a22324d341cac89c0e
madi ! <madiggudin@gmail.com>
2023-11-11 21:03:41 -0500

    big refactor moment

--- dialog_box.py
+++ dialog_box.py
@@ -1,36 +1,35 @@
 import pygame as pg
 from pygame import sprite
 
-class DialogBox:
-    def __init__(self, x: int, y: int, width: int, height: int, lines, world):
+
+class DialogBox(pg.sprite.Sprite):
+    def __init__(self, x: int, y: int, width: int, height: int, lines: list[str], world):
+        pg.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.world = world
         
         self.lines = lines
         self.lines_index = 0
         self.lines_count = len(lines)
-
-        self.world = world
         
         self.font = pg.font.Font("./assets/november.ttf", 25)
-        self.text_surface = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
-        
+
+        self.image = pg.Surface((self.width, self.height))
         self.rect = pg.Rect(self.x, self.y, self.width, self.height)
         
-    def draw(self, surface):
-        pg.draw.rect(surface, (255, 255, 255), self.rect)
-        surface.blit(self.text_surface, (self.x + 5, self.y + 5))
-        
     def update(self):
-        pass
+        self.text = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
+        self.image.fill((255, 255, 255))
+        self.image.blit(self.text, (5, 5))
 
-    def check_click(self, p):
+    def check_click(self, p: tuple[int, int]):
         if self.rect.collidepoint(p):
             self.lines_index += 1
             if self.lines_index >= self.lines_count:
-                self.world.world_dboxes.remove(self)
+                self.world.sprites.remove(self)
                 self.world.state = "idle_main_room"
             else:
                 self.text_surface = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
