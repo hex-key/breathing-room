74c1e6fd4c9f3174ded282b5f8bf1b4d637b4904
T-FWK <irischen0505@gmail.com>
2023-11-11 23:04:35 -0500

    brief indent fix

---
+++ choice_box.py
@@ -0,0 +1,8 @@
+"""
+general things this needs to do: 
+-- appear when the text box reaches a certain state (ie one where choices are required)
+-- display two choices
+-- the player can select between them using arrow keys/spacebar or by clicking
+-- selected choice denoted by a ">" symbol and/or a different text color
+-- once the player makes a choice, signal the dialog/story to move on and the box to disappear
+"""
