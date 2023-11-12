ebb94d23a2811cd73402b662ed26e17818490194
T-FWK <irischen0505@gmail.com>
2023-11-11 22:21:53 -0500

    HOLY FRICKIN FINALLY

--- dialog_box.py
+++ dialog_box.py
@@ -21,9 +21,16 @@ class DialogBox(pg.sprite.Sprite):
         self.rect = pg.Rect(self.x, self.y, self.width, self.height)
         
     def update(self):
-        self.text = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
+        line_segments = break_line(self, self.lines[self.lines_index])
         self.image.fill((255, 255, 255))
-        self.image.blit(self.text, (5, 5))
+        height_index = 0
+        for segment in line_segments:
+            text = self.font.render(segment, True, (0, 0, 0))
+            self.image.blit(text, (5, 5 + ((self.font.get_height()+2)*height_index)))
+            # self.image.blit(text, (5, 5))
+            height_index += 1
+            
+            print(height_index)
 
     def check_click(self, p: tuple[int, int]):
         if self.rect.collidepoint(p):
@@ -34,11 +41,11 @@ class DialogBox(pg.sprite.Sprite):
             else:
                 self.text_surface = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
 
-def break_lines(self, line):
+def break_line(self, line):
         """
         Breaks a line of text into multiple lines that fit within the width of the box, respecting word boundaries. Returns list[str] of these lines. 
         """
-        broken_lines = ""
+        broken_lines = []
         current_segment = ""
         working_line = line.split(" ")
 
@@ -46,13 +53,13 @@ def break_lines(self, line):
             # see if the current segment still fits if one more word is added to it
             if self.font.render(current_segment + " " + working_line[0], True, (0, 0, 0)).get_width() >= self.width - 10:
                 # if not, add a break and reset the current segment
-                broken_lines += (current_segment + "\n")
+                broken_lines.append(current_segment + " ")
                 current_segment = ""
-                print("1 - current segment: " + current_segment)
+
             else:
                 # else, add another word
                 current_segment += " " + working_line[0]
                 working_line = working_line[1:]
-                print("2 - current segment: " + current_segment)
+        broken_lines.append(current_segment)
 
-        return broken_lines.strip("\n")
+        return broken_lines
