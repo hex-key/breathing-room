83829a660879a4c9035e9b9d19fd1e4bbe5a92cb
T-FWK <irischen0505@gmail.com>
2023-11-11 21:06:00 -0500

    resolve conflicts w/ master

--- dialog_box.py
+++ dialog_box.py
@@ -33,3 +33,26 @@ class DialogBox(pg.sprite.Sprite):
                 self.world.state = "idle_main_room"
             else:
                 self.text_surface = self.font.render(self.lines[self.lines_index], True, (0, 0, 0))
+
+def break_lines(self, line):
+        """
+        Breaks a line of text into multiple lines that fit within the width of the box, respecting word boundaries. Returns list[str] of these lines. 
+        """
+        broken_lines = ""
+        current_segment = ""
+        working_line = line.split(" ")
+
+        while len(working_line) > 0:
+            # see if the current segment still fits if one more word is added to it
+            if self.font.render(current_segment + " " + working_line[0], True, (0, 0, 0)).get_width() >= self.width - 10:
+                # if not, add a break and reset the current segment
+                broken_lines += (current_segment + "\n")
+                current_segment = ""
+                print("1 - current segment: " + current_segment)
+            else:
+                # else, add another word
+                current_segment += " " + working_line[0]
+                working_line = working_line[1:]
+                print("2 - current segment: " + current_segment)
+
+        return broken_lines.strip("\n")
